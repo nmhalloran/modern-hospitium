ebe7cdf3cfba08a56eb88422cbc92dbd498d38ff
Nicholas Halloran <nmhalloran@gmail.com>
2018-07-09 20:26:34 -0700

    heroku fixes

--- frontend/components/home_page/root_page.jsx
+++ frontend/components/home_page/root_page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import {closeModal } from '../actions/modal_actions'
 
 class RootPage extends React.Component {
   render() {
